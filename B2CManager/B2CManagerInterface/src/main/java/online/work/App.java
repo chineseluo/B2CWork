9259ff30c72edd4f23ea0a1d517d9f56c290d8a9
chineseluo <848257135@qq.com>
2018-02-08 22:50:47 +0800

    Initial commit

---
+++ B2CManager/B2CManagerInterface/src/main/java/online/work/App.java
@@ -0,0 +1,13 @@
+package online.work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
